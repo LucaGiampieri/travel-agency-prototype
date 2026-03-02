e8fb622d4922558b36a3e3ae5a468e7250a7f98f
Luca Giampieri <luca.giampieri95@gmail.com>
2026-03-02 12:29:32 +0100

    Inizio Filitro

--- src/pages/TravelPage.jsx
+++ src/pages/TravelPage.jsx
@@ -1,5 +1,7 @@
 import { useParams } from "react-router-dom"
 
+import { useState } from "react";
+
 import travels from "../data/Travels";
 
 import { Link } from "react-router-dom";
@@ -10,20 +12,46 @@ function TravelPage() {
 
     const partecipantiViaggio = travels.find(part => (part.id === parseInt(id)))
 
+    const [formData, setFormData] = useState("");
+
+
+    function setFieldValue(e) {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+
     return (
 
         <>
-            <input type="text" placeholder="Chi stai cercando?" />
-            <ul>
-                {partecipantiViaggio.partecipanti.map((part, index) => {
-                    return (
-                        <li key={index}>
-                            <strong>Nome: {part.nome} {part.cognome}</strong>
-                            <div>Telefono: {part.telefono}</div>
-                            <div>Email: {part.email}</div>
+            <input
+                name="name"
+                type="text"
+                placeholder="Chi stai cercando?"
+                onChange={setFieldValue}
 
-                        </li>
-                    )
+            />
+
+
+
+
+
+            <ul>
+                {partecipantiViaggio.partecipanti.nome.filter(partName => {
+                    if (partName === formData)
+                        return (
+                            partecipantiViaggio.partecipanti.map((part, index) => {
+                                return (
+                                    <li key={index}>
+                                        <strong>Nome: {part.nome} {part.cognome}</strong>
+                                        <div>Telefono: {part.telefono}</div>
+                                        <div>Email: {part.email}</div>
+
+                                    </li>
+                                )
+                            }))
                 })}
             </ul>
 
